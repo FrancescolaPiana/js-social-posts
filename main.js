416ed6b9656c383b739950f888cbfad51d34c069
Francescolapiana <Lushika1219@gmail.com>
2022-10-24 17:38:55 +0200

    Hotwell

--- main.js
+++ main.js
@@ -60,40 +60,42 @@ const father = document.getElementById('container')
 // main function generate html
 
 
-let post = document.createElement('div')
- post.className = "post"
- post.innerHTML = `
- <div class="post__header">
- <div class="post-meta">                    
-     <div class="post-meta__icon">
-         <img class="profile-pic" src="https://unsplash.it/300/300?image=15" alt="Phil Mangione">                    
-     </div>
-     <div class="post-meta__data">
-         <div class="post-meta__author">Phil Mangione</div>
-         <div class="post-meta__time">4 mesi fa</div>
-     </div>                    
- </div>
-</div>
-<div class="post__text">Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.</div>
-<div class="post__image">
- <img src="https://unsplash.it/600/300?image=171" alt="">
-</div>
-<div class="post__footer">
- <div class="likes js-likes">
-     <div class="likes__cta">
-         <a class="like-button  js-like-button" href="#" data-postid="1">
-             <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
-             <span class="like-button__label">Mi Piace</span>
-         </a>
-     </div>
-     <div class="likes__counter">
-         Piace a <b id="like-counter-1" class="js-likes-counter">80</b> persone
-     </div>
- </div> 
-</div>    
-`
- let value;
-    father.append(post);
+
+
+    let value;
 posts.forEach(value,index => {
- 
+    let post = document.createElement('div')
+    post.className = "post"
+    post.innerHTML = `
+    <div class="post__header">
+    <div class="post-meta">                    
+        <div class="post-meta__icon">
+            <img class="profile-pic" src="https://unsplash.it/300/300?image=15" alt="Phil Mangione">                    
+        </div>
+        <div class="post-meta__data">
+            <div class="post-meta__author">Phil Mangione</div>
+            <div class="post-meta__time">4 mesi fa</div>
+        </div>                    
+    </div>
+   </div>
+   <div class="post__text">Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.</div>
+   <div class="post__image">
+    <img src="https://unsplash.it/600/300?image=171" alt="">
+   </div>
+   <div class="post__footer">
+    <div class="likes js-likes">
+        <div class="likes__cta">
+            <a class="like-button  js-like-button" href="#" data-postid="1">
+                <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
+                <span class="like-button__label">Mi Piace</span>
+            </a>
+        </div>
+        <div class="likes__counter">
+            Piace a <b id="like-counter-1" class="js-likes-counter">80</b> persone
+        </div>
+    </div> 
+   </div>    
+   `
+   
+       father.append(post);
 });
